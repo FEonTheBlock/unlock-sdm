67d798d1a0cf43b0bd6f4621f34a78f7190e15ba
hhhyyo <gydnjsrh6923@korea.ac.kr>
2023-06-18 05:44:34 +0900

    feat: 디테일 페이지 마크업

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -1,8 +1,17 @@
 import { supabase } from '$lib/supabaseClient';
 
-export async function load() {
-	const { data } = await supabase.from('sdm-infos').select();
+export async function load({ params }) {
+	const { data } = await supabase.from('sdm-infos').select().eq('id', params.id).limit(1).single();
+
+	console.log(data);
+
 	return {
-		sdmInfos: data ?? []
+		consultingDate: data.consultingDate,
+		weddingDate: data.weddingDate,
+		studioInfo: data.studioInfo,
+		dressInfo: data.dressInfo,
+		makeUpInfo: data.makeUpInfo,
+		consultingInfo: data.consultingInfo,
+		totalPrice: data.totalPrice
 	};
 }
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,11 +1,97 @@
 <script>
+	import { formatPrice } from '$utils/formatUtils';
+
 	export let data;
-	let { sdmInfos } = data;
-	$: ({ sdmInfos } = data);
+	let { studioInfo, dressInfo, makeUpInfo, consultingInfo, totalPrice, consultingDate, weddingDate } = data;
+	$: ({ studioInfo, dressInfo, makeUpInfo, consultingInfo, consultingDate, weddingDate } = data);
 </script>
 
-<ul>
-	{#each sdmInfos as sdmInfo}
-		<li>{sdmInfo.dress_price}</li>
-	{/each}
-</ul>
+<div>
+	{formatPrice(totalPrice)}
+</div>
+<dl>
+	<div>
+		<dt>상담/견적일</dt>
+		<dd>{consultingDate}</dd>
+	</div>
+	<div>
+		<dt>본식 날짜</dt>
+		<dd>{weddingDate}</dd>
+	</div>
+</dl>
+<dl>
+	<div>
+		<dt>스튜디오</dt>
+		<dd>{studioInfo.name}</dd>
+	</div>
+	<div>
+		<dt>비용</dt>
+		<dd>{formatPrice(studioInfo.price)}</dd>
+	</div>
+	<div>
+		<dt>비고</dt>
+		<dd>{studioInfo.comment}</dd>
+	</div>
+</dl>
+<dl>
+	<div>
+		<dt>드레스</dt>
+		<dd>{dressInfo.name}</dd>
+	</div>
+	<div>
+		<dt>비용</dt>
+		<dd>{formatPrice(dressInfo.price)}</dd>
+	</div>
+	<div>
+		<dt>비고</dt>
+		<dd>{dressInfo.comment}</dd>
+	</div>
+</dl>
+<dl>
+	<div>
+		<dt>메이크업</dt>
+		<dd>{makeUpInfo.name}</dd>
+	</div>
+	<div>
+		<dt>비용</dt>
+		<dd>{formatPrice(makeUpInfo.price)}</dd>
+	</div>
+	<div>
+		<dt>비고</dt>
+		<dd>{makeUpInfo.comment}</dd>
+	</div>
+</dl>
+<dl>
+	<div>
+		<dt>플래너</dt>
+		<dd>{consultingInfo.name}</dd>
+	</div>
+	<div>
+		<dt>비용</dt>
+		<dd>{formatPrice(consultingInfo.price)}</dd>
+	</div>
+	<div>
+		<dt>비고</dt>
+		<dd>{consultingInfo.comment}</dd>
+	</div>
+</dl>
+
+<style>
+	dl > div{
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	dl:not(:last-child)::after{
+		display: block;
+		content: "";
+		width: 90%;
+		height: 1px;
+		margin: 0 auto;
+		border-bottom: 1px dashed var(--light-gray);
+	}
+
+	dt{
+		font-weight: bold;
+	}
+</style>
