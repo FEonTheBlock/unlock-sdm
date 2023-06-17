3bac5aa4637e6f099e18dbd01765a8577dbab479
hanana1253 <hanana1253@gmail.com>
2023-06-18 01:31:18 +0900

    conf: Initialize supabase and sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let data;
+	let { sdmPrices } = data;
+	$: ({ sdmPrices } = data);
+</script>
+
+<ul>
+	{#each sdmPrices as sdmPrice}
+		<li>{sdmPrice.consulting_name}</li>
+	{/each}
+</ul>
