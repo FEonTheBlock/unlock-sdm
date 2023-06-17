7b7d8ca906285f852573b46d7776bf4235929fba
hhhyyo <gydnjsrh6923@korea.ac.kr>
2023-06-18 04:56:21 +0900

    feat: 영수증 레이아웃 및 디테일페이지 라우팅

---
+++ src/routes/[id]/+layout.svelte
@@ -0,0 +1,48 @@
+<div class="printer">
+  <div class="receipt">
+		<slot />
+  </div>
+</div>
+
+<style>
+.printer{
+  padding-top: 10px;
+}
+
+.printer::before{
+  display: block;
+  content: "";
+  position: relative;  
+  width: 95%;
+  height: 14px;
+  margin: 0 auto;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+}
+
+.receipt {
+  width: 85%;
+  height: 92vh;
+  position: relative;
+  top: -12px;
+  margin: 0 auto;
+  background: white;
+  filter:drop-shadow(0px 0px 1px gray);
+}
+
+.receipt:after{
+  display: block;
+  content: "";
+	width: 100%;
+	height: 32px;
+  position: absolute;
+  bottom: -15px;
+  background: 
+    linear-gradient(-45deg, transparent 16px, white 0), 
+		linear-gradient(45deg, transparent 16px, white  0);
+  background-repeat: repeat-x;
+	background-position: left bottom;
+  background-size: 22px 32px;  
+}
+</style>
---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let data;
+	let { sdmInfos } = data;
+	$: ({ sdmInfos } = data);
+</script>
+
+<ul>
+	{#each sdmInfos as sdmInfo}
+		<li>{sdmInfo.dress_price}</li>
+	{/each}
+</ul>
